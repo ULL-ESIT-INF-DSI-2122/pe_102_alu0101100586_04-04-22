4b944fe3e41dd700469c3d55f69f18aad6c3c574
usuario <alu0101100586@ull.edu.es>
2022-04-04 09:36:49 +0000

    Correcion de los badges

--- tests/sort_strategy.spec.ts
+++ tests/sort_strategy.spec.ts
@@ -4,8 +4,8 @@ import { Sort_Strategy } from '../src/context/sort';
 import { Bubble_Sort } from '../src/algorithm/bubble';
 import { Merge_Sort } from '../src/algorithm/merge';
 
-let data: number[] = [9, 8, 7, 5, 4, 6, 2, 1, 3];
-let data_sort:  number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let data: number[] = [9, 8, 7, 5, 4, 6, 2, 1, 3, 10, 40, 50];
+let data_sort:  number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 40, 50];
 let test = new Sort_Strategy(data, new Bubble_Sort());
 
 describe("Sort using  Strategy", () => {
